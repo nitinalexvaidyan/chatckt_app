726fd9b832e76df0d736c1d122f59723f46678b8
nitinalexvaidyan <nitin.alex@shopalyst.com>
2024-11-25 23:46:04 +0530

    second commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to your Flask App!"
+    return "Hello from Flask on AWS EC2!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
