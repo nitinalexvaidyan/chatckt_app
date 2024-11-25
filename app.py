93b180f5afaadd458972e51dd84ac3db8100e51c
nitinalexvaidyan <nitin.alex@shopalyst.com>
2024-11-26 00:00:50 +0530

    fifth commit

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello from Flask on AWS EC2!"
 
+@app.route('/nitin')
+def nitin():
+    return "Hello from Nitin Alex Vaidyan"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
