4c77bcb5e3f916341736a6b2c0ca96042bc1b567
nitinalexvaidyan <nitin.alex@shopalyst.com>
2024-11-28 13:36:24 +0530

    12th commit

--- batch_jobs/test/load_test_data.py
+++ batch_jobs/test/load_test_data.py
@@ -1,16 +1,20 @@
 from elasticsearch import Elasticsearch
 import json
 
+print("starting load_test_data.py !!!... ")
 # Connect to Elasticsearch
-es = Elasticsearch("http://your-ec2-public-ip:9200")  # Replace with your EC2 IP and port
+es = Elasticsearch("http://localhost:9200")  # Replace with your EC2 IP and port
 
+print("Got es client")
 # Load test data from JSON file
 with open('test_data.json', 'r') as file:
     test_data = json.load(file)
 
+print("________test_data ______\n", test_data)
 # Index each document
 index_name = 'chat_messages'
 for doc in test_data:
+    print("Indexing the data")
     es.index(index=index_name, id=doc['id'], document=doc)
 
 print("Test data successfully indexed!")
