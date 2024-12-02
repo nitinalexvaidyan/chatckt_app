e377dbe8e8ef2e3530f393e3f84b783a6d12618f
nitinalexvaidyan <nitin.alex@shopalyst.com>
2024-12-02 08:58:47 +0530

    open api int

--- chat_api/model.py
+++ chat_api/model.py
@@ -1,4 +1,9 @@
+from utils import nls_to_dsl_util
+from utils import es_util
+
 def get_query_answer(query):
-    answer = query[::-1]
-    print(f"Saving message to the database: {query}")
-    return answer
+    es_query = nls_to_dsl_util.get_dsl_query(query)
+    es_response = es_util.search_data("cricket_matches", es_query)
+    final_response = nls_to_dsl_util.get_final_response(es_response, query)
+    print(f"final_response: {final_response}")
+    return final_response
