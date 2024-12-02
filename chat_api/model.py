ffc996723c1bfde4d5e60951fde3e66af1e5e872
nitinalexvaidyan <nitin.alex@shopalyst.com>
2024-12-02 09:21:23 +0530

    print

--- chat_api/model.py
+++ chat_api/model.py
@@ -7,7 +7,9 @@ from utils import es_util
 
 def get_query_answer(query):
     es_query = nls_to_dsl_util.get_dsl_query(query)
+    print("es_query", es_query)
     es_response = es_util.search_data("cricket_matches", es_query)
+    print("es_response", es_response)
     final_response = nls_to_dsl_util.get_final_response(es_response, query)
     print(f"final_response: {final_response}")
     return final_response
